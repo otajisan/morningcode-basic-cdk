b8bc722d48678a3b1a2ce9d4641407b6c593a213
otajisan <taji.taji.otajisan@gmail.com>
2022-12-26 14:20:48 +0900

    :up: upgrade to aws cdk v2

--- lib/morningcode-basic-cdk-stack.ts
+++ lib/morningcode-basic-cdk-stack.ts
@@ -1,39 +1,94 @@
-import * as cdk from '@aws-cdk/core';
-import {Tags} from "@aws-cdk/core";
-import {SubnetType, Vpc} from "@aws-cdk/aws-ec2";
+import { Stack, StackProps, Tags } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {
+  CfnEIP,
+  CfnInternetGateway,
+  CfnNatGateway,
+  CfnVPCGatewayAttachment, IpAddresses,
+  RouterType,
+  Subnet,
+  Vpc,
+} from 'aws-cdk-lib/aws-ec2';
 
-export class MorningcodeBasicCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class MorningcodeBasicCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const vpc = new Vpc(this, 'Vpc', {
       vpcName: 'morningcode-main-vpc',
       maxAzs: 2,
-      cidr: '192.168.0.0/16',
-      natGateways: 1,
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: 'SubnetPub1',
-          subnetType: SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 24,
-          name: 'SubnetPub2',
-          subnetType: SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 24,
-          name: 'SubnetPri1',
-          subnetType: SubnetType.PRIVATE_WITH_NAT,
-        },
-        {
-          cidrMask: 24,
-          name: 'SubnetPri2',
-          subnetType: SubnetType.PRIVATE_WITH_NAT,
-        },
-      ]
+      ipAddresses: IpAddresses.cidr('192.168.0.0/16'),
+      subnetConfiguration: []
     });
+
+    // Public Subnets
+    const publicSubnet1 = new Subnet(this, 'PublicSubnet1a', {
+      availabilityZone: 'ap-northeast-1a',
+      vpcId: vpc.vpcId,
+      cidrBlock: '192.168.0.0/24',
+    });
+
+    // const publicSubnet2 = new Subnet(this, 'PublicSubnet1c', {
+    //   availabilityZone: 'ap-northeast-1c',
+    //   vpcId: vpc.vpcId,
+    //   cidrBlock: '192.168.1.0/24',
+    // });
+
+    // Private Subnets
+    const privateSubnet1 = new Subnet(this, 'PrivateSubnet1a', {
+      availabilityZone: 'ap-northeast-1a',
+      vpcId: vpc.vpcId,
+      cidrBlock: '192.168.10.0/24',
+    });
+
+    // const privateSubnet2 = new Subnet(this, 'PrivateSubnet1c', {
+    //   availabilityZone: 'ap-northeast-1c',
+    //   vpcId: vpc.vpcId,
+    //   cidrBlock: '192.168.11.0/24',
+    // });
+
+    // Internet Gateway
+    const internetGateway = new CfnInternetGateway(this, 'InternetGateway', {});
+
+    // Add route to Public Subnets
+    // NOTE: to access Public Subnets from the external
+    new CfnVPCGatewayAttachment(this, 'gateway', {
+      vpcId: vpc.vpcId,
+      internetGatewayId: internetGateway.ref,
+    });
+
+    publicSubnet1.addRoute('PublicSubnetRoute', {
+      routerType: RouterType.GATEWAY,
+      routerId: internetGateway.ref,
+    });
+
+    // publicSubnet2.addRoute('PublicSubnetRoute', {
+    //   routerType: RouterType.GATEWAY,
+    //   routerId: internetGateway.ref,
+    // });
+
+    // EIP for NAT Gateway
+    const eip1 = new CfnEIP(this, 'ElasticIP1', {});
+    //const eip2 = new CfnEIP(this, 'ElasticIP2', {});
+
+    // NAT Gateway
+    const natGateway1 = new CfnNatGateway(this, 'NatGateway1', {
+      allocationId: eip1.attrAllocationId,
+      subnetId: publicSubnet1.subnetId,
+    });
+    // const natGateway2 = new CfnNatGateway(this, 'NatGateway2', {
+    //   allocationId: eip2.attrAllocationId,
+    //   subnetId: publicSubnet2.subnetId,
+    // });
+
+    privateSubnet1.addRoute('PrivateSubnetRoute1', {
+      routerType: RouterType.NAT_GATEWAY,
+      routerId: natGateway1.ref,
+    });
+    // privateSubnet2.addRoute('PrivateSubnetRoute2', {
+    //   routerType: RouterType.NAT_GATEWAY,
+    //   routerId: natGateway2.ref,
+    // });
 
     Tags.of(this).add('ServiceName', 'morningcode');
   }
--- test/morningcode-basic-cdk.test.ts
+++ test/morningcode-basic-cdk.test.ts
@@ -1,13 +1,71 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as MorningcodeBasicCdk from '../lib/morningcode-basic-cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import {Match, Template} from 'aws-cdk-lib/assertions';
+import {MorningcodeBasicCdkStack} from "../lib/morningcode-basic-cdk-stack";
 
-test('Empty Stack', () => {
+describe('correct resources are created', () => {
+  const synthStack = () => {
     const app = new cdk.App();
-    // WHEN
-    const stack = new MorningcodeBasicCdk.MorningcodeBasicCdkStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT));
+    const env = {
+      account: process.env.CDK_DEPLOY_ACCOUNT,
+      region: process.env.CDK_DEPLOY_REGION,
+    };
+    return new MorningcodeBasicCdkStack(app, 'MorningcodeBasicCdkStack', {
+      env,
+    });
+  };
+
+  const getResourceIds = (type: string, props?: any): string[] =>
+    Object.keys(Template.fromStack(synthStack()).findResources(type, props ? {Properties: props} : {}));
+
+  const getResourceId = (type: string, props?: any): string => getResourceIds(type, props)[0];
+
+  const vpcId = getResourceId('AWS::EC2::VPC');
+
+  test('VPC is created', () => {
+    const template = Template.fromStack(synthStack());
+
+    const expectedTags = [
+      {
+        'Key': 'Name',
+        'Value': 'morningcode-main-vpc',
+      },
+      {
+        'Key': 'ServiceName',
+        'Value': 'morningcode',
+      }
+    ];
+
+    template.hasResourceProperties('AWS::EC2::VPC', {
+      Tags: expectedTags
+    })
+  });
+
+  test('Subnets are created', () => {
+    const template = Template.fromStack(synthStack());
+
+    template.resourceCountIs('AWS::EC2::Subnet', 2);
+    template.hasResourceProperties('AWS::EC2::Subnet', {
+      VpcId: { Ref: vpcId },
+      AvailabilityZone: 'ap-northeast-1a',
+      CidrBlock: Match.stringLikeRegexp('.*/24'),
+    })
+  });
+
+  test('InternetGateways are created', () => {
+    const template = Template.fromStack(synthStack());
+
+    template.resourceCountIs('AWS::EC2::InternetGateway', 1);
+  });
+
+  test('Elastic IPs are created', () => {
+    const template = Template.fromStack(synthStack());
+
+    template.resourceCountIs('AWS::EC2::EIP', 1);
+  });
+
+  test('NAT Gateways are created', () => {
+    const template = Template.fromStack(synthStack());
+
+    template.resourceCountIs('AWS::EC2::NatGateway', 1);
+  });
 });
